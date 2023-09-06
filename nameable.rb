5550020525dbef8752d314950e2f7b714d4dcd82
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-06 18:19:32 +0300

    update nameable

--- nameable.rb
+++ nameable.rb
@@ -1,6 +1,6 @@
 class Nameable
     def correct_name
-      raise NotImplementedError, "#{self.class} has not implemented the 'correct_name' method."
+      raise NotImplementedError
     end
   end
   

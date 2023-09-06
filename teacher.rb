956647bd03382f7427447d7a45792d53a6842337
Henok Mekonnen <henokme@ethiopianairlines.com>
2023-09-06 18:19:46 +0300

    update teacher

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(name = 'Unknown', specialization, age = 0)
+  def initialize(name,age = 'Unknown', specialization )
     super(name, age)
     @specialization = specialization
   end

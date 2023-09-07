9ac841fb91717768aec6a324677a717c3de96d84
Henok Mekonnen <henokme@ethiopianairlines.com>
2023-09-07 22:29:43 +0300

    update student rb file

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(name = 'Unknown', parent_permission: true, classroom = nil, age = 0)
-    super(name, age, parent_permission)
+  def initialize(name, age, classroom, parent_permission: true)
+    super(name, age, parent_permission: parent_permission)
     self.classroom = classroom
   end
 
